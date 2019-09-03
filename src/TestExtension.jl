277848d99450149087bae48820db70917f466d0a
Tobias Schoels <tobias.schoels@gmail.com>
2019-09-03 12:03:08 +0200

    initial commit

---
+++ src/TestExtension.jl
@@ -0,0 +1,38 @@
+module TestExtension
+using Test
+
+
+import Test: @test, test_expr!, get_test_result, do_test, Returned
+
+"""
+    @test test_expr error_expr
+    @test f(args...) error_expr key=val...
+
+Exactly like `@test`, but evaluates `error_expr` if `@test` did not `Pass`.  
+
+# Examples
+```jldoctest
+julia> @test true 37+5
+Test Passed
+julia> @test false 37+5
+[ Info: 42
+Test Failed at REPL[34]:1
+  Expression: false
+ERROR: There was an error during testing
+```
+"""
+macro test(ex, msg, kws...)
+    test_expr!("@test", ex, kws...)
+    orig_ex = Expr(:inert, ex)
+    result = get_test_result(ex, __source__)
+    quote
+        if $(isa(result, Returned) && isa(result.value, Bool) && !result.value) 
+            @info $msg
+        end
+        do_test($result, $orig_ex)
+    end
+end
+
+export @test
+
+end # module
